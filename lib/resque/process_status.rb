88d436135edf35f98fdfea7d3470733d8556f58d
Olexandr Hoshylyk <gashuk95@gmail.com>
2020-07-13 11:27:24 +0300

    Init

---
+++ lib/resque/process_status.rb
@@ -0,0 +1,8 @@
+require "resque/process_status/version"
+
+module Resque
+  module ProcessStatus
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/resque/process_status/version.rb
@@ -0,0 +1,5 @@
+module Resque
+  module ProcessStatus
+    VERSION = "0.1.0"
+  end
+end
